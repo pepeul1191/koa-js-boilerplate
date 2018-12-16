9de2341ab72e5e1014cbf1e2741392ab9c506228
Pepe Valdivia <jvaldivia@softweb.pe>
2018-12-16 04:09:28 -0500

    Routes

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const Koa = require('koa');
 const static = require('koa-static');
+const routes = require('koa-route');
 
 const app = new Koa();
 
 app.use(static(__dirname + '/public'));
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
+const _r = {
+  home: (ctx) => {
+    ctx.body = 'pets';
+  },
+};
+
+app.use(routes.get('/', _r.home));
 
 app.listen(3000);

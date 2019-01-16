412355fa4a2d796c2d9416d4be845a9755612f39
Pepe Valdivia <jvaldivia@softweb.pe>
2019-01-16 10:42:44 -0500

    Middleware de logs

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const routes = require('koa-route');
 
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
 app.use(static(__dirname + '/public'));
 
 const _r = {
   home: (ctx) => {
-    ctx.body = 'pets';
+    ctx.body = 'petsss';
   },
 };
 

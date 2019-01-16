4196cea8261b7c3d2553e0601399ae14045b146f
Pepe Valdivia <jvaldivia@softweb.pe>
2019-01-16 10:53:15 -0500

    Middleware en otro archivo

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const Koa = require('koa');
 const static = require('koa-static');
 const routes = require('koa-route');
+var middleware = require('./configs/middlewares');
 
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.get('X-Response-Time');
-  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-});
+app.use(middleware.preResponse());
 
 app.use(static(__dirname + '/public'));
 
 const _r = {
   home: (ctx) => {
-    ctx.body = 'petsss';
+    ctx.set('Content-Type', 'text/html');
+    ctx.body = '<h1>petsss</h1>';
   },
 };
 
---
+++ configs/middlewares.js
@@ -0,0 +1,9 @@
+function preResponse(){
+  return async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+  }
+}
+
+exports.preResponse= preResponse;

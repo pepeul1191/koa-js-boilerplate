5939f1937722fe4e77317faeada32d64ff811278
V5623 <V5623@lab.aca>
2019-07-11 16:58:14 -0500

    Mandnando id de usuario en conversacion

--- sockets/chat.js
+++ sockets/chat.js
@@ -4,14 +4,16 @@ const io = new IO({
   namespace: 'chat'
 });
 
-io.on('connection', function (socket) {
-  console.log(socket.data.handshake.query.user_id);
-  console.log('usuario conectado');
+io.on('connection', function (ctx) {
+  ctx.socket.emit('wellcome message', 'Bienvenido ' + ctx.data.handshake.query.user_id);
+  console.log('usuario ' + ctx.data.handshake.query.user_id + ' conectado');
 });
 
 io.on('chat message', function(ctx, data){
   console.log(data);
-  ctx.socket.broadcast.emit('chat message', data);
+  console.log();
+  var msg = ctx.socket.handshake.query.user_id + ' dice: ' + data;
+  ctx.socket.broadcast.emit('chat message', msg);
 });
 
 io.on('disconnect', function () {

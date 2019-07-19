d28e48faf04c6cded02a83c41b4b0b94fe55c392
Pepe Valdivia <jvaldivia@softweb.pe>
2019-07-19 07:39:25 -0500

    Models de access

--- configs/models.js
+++ configs/models.js
@@ -1,15 +1,58 @@
 var db = require('./database');
 
+// access's models
+
+var UserSystem = new db.Schema({
+  system_id: db.Schema.Types.ObjectId,
+  permissions_id: [db.Schema.Types.ObjectId],
+});
+
 var User = db.mongoose.model('users',
-  new db.Schema(
-    {
-      user:  String,
-      pass: String,
-      email: String,
-      profile_picture: String,
-      status: String,
-    }
-  )
+  new db.Schema({
+    user:  String,
+    pass: String,
+    email: String,
+    profile_picture: String,
+    systems: [UserSystem], 
+  })
 );
 
+var State = db.mongoose.model('states',
+  new db.Schema({
+    name:  String,
+  })
+);
+
+var Log = db.mongoose.model('logs',
+  new db.Schema({
+    user_id:  db.Schema.Types.ObjectId,
+    time:  db.Schema.Types.Date,
+    action:  String,
+    detail:  String,
+  })
+);
+
+var Permission = db.mongoose.model('permission',
+  new db.Schema({
+    name: String,
+    key: String, 
+  })
+);
+
+var System = db.mongoose.model('systems',
+  new db.Schema({
+    name: String,
+    permissions_id: [db.Schema.Types.ObjectId],
+  })
+);
+
+// chat's models
+
+// ---
+
 exports.User = User;
+exports.State = State;
+exports.Log = Log;
+exports.Permission = Permission;
+exports.System = System;
+exports.UserSystem = UserSystem;
